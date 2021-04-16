bb93cce377ecefcd31f66f9024c12c62ee0a8fd0
Hussein Elsherbini <hae26@scarletmail.rutgers.edu>
2021-04-15 22:14:34 -0400

    continued updating the add device dialog functionality

--- detectUsb.py
+++ detectUsb.py
@@ -1,14 +1,22 @@
-from winrt.windows.devices.enumeration import DeviceInformation
-from winrt.windows.devices.usb import UsbDevice
-from winrt.windows.devices import usb
-from winrt import *
+
+from pysetupdi import setupdi
+
+
 
 class detectUsb:
 
     def driveStatus(self):
-        #u =
-        #print(u)
-        print(DeviceInformation.find_all_async(UsbDevice.get_device_class_selector(usb.UsbDeviceClasses.cdc)))
+        u = []
+        devs = setupdi.devices(enumerator = "USB")
+
+        for x in devs:
+            name = x.device_class[:7]
+            print(name)
+            if name == "USBTest":
+                u.append(x)
+
+
+
 
 u = detectUsb()
 
