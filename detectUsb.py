2f3f35303c6fbdd9bab3481781ec66bc32057800
hussein elsherbini <hae26@scarletmail.rutgers.edu>
2021-04-22 22:45:32 -0400

    yh

--- detectUsb.py
+++ detectUsb.py
@@ -1,43 +1,62 @@
 import pyvisa
 from pysetupdi import setupdi
+import usb.core
+import usb.backend.libusb1
 import sys
+import usbtmc
 
 devices = []
+devices_VID_PID = []
 devs = setupdi.devices()
+
 for dev in devs:
     try:
         if dev.enumerator_name == "USB":
             if dev.device_class == "Bluetooth" or dev.device_class == "HIDClass" or dev.device_class == "Net" or dev.device_class == "MEDIA":
                 pass
-            elif "HUB" in dev.manufacturer or "Controller" in dev.manufacturer or "storage" in dev.manufacturer:
-                print(dev.manufacturer)
+            elif "HUB" in dev.manufacturer or "Hub" in dev.device_desc or "Controller" in dev.manufacturer or "storage" in dev.manufacturer:
+                # print(dev.manufacturer)
                 pass
             else:
                 devices.append(dev)
 
-    except (KeyError,AttributeError,):
+    except (KeyError, AttributeError,):
         continue
 
 print("\n")
 for x in devices:
     try:
-        print("Device name: {}   Device class: {}".format(x,x.device_class))
+        print("Device name: {}   Device class: {}".format(x, x.manufacturer))
+        h = x.hardware_id
+        devices_VID_PID.append(("0x"+h[1][8:12], "0x"+h[1][17:]))
+
     except AttributeError:
         pass
-class detectUsb:
+
+print(devices_VID_PID)
+
+
+class device:
+
+    def __init__(self, vid, pid):
+
+        self.VID = vid
+        self.PID = pid
+        self.dev = usbtmc.list_devices()
+
 
     def driveStatus(self):
         resourceManager = pyvisa.ResourceManager()
-        #instrument = resourceManager.open_resource("TCPIP::169.254.202.45::INSTR")
+        # instrument = resourceManager.open_resource("TCPIP::169.254.202.45::INSTR")
 
         print(resourceManager.list_resources())
 
         for device in resourceManager.list_resources():
             print(device)
             dev = resourceManager.open_resource(device)
-            print(dev.query('*IDN?'))
+            #print(dev.query('*IDN?'))
 
 
-u = detectUsb()
-
-#u.driveStatus()
+u = device(devices_VID_PID[1][0], devices_VID_PID[1][1])
+u.driveStatus()
+print(u.dev)
